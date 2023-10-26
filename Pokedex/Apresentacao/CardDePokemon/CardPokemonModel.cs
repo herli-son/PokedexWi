890a530e97aac33f48544802e80f5eba718c3674
ReHenrique <renanhenriquegj2004@gmail.com>
2023-10-26 09:54:22 -0300

    Criado card Pokemon Model

---
+++ Pokedex/Apresentacao/CardDePokemon/CardPokemonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apresentacao.CardDePokemon
+{
+    public class CardPokemonModel
+    {
+        public string Nome { get; set; }
+        public string Numero { get; set; }
+        public string TipoPrimario { get; set; }
+        public string TipoSecundario { get; set; }
+        public string Historia { get; set; }
+
+
+    }
