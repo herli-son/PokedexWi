4309fbd3134057db2084c36d82fe180e13ce5256
Kaio BJ <kaioresendeoficial@outlook.com>
2023-10-26 10:05:13 -0300

    Create View CardDePokemon

---
+++ Pokedex/Apresentacao/CardDePokemon/CardDePokemonView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apresentacao
+{
+    /// <summary>
+    /// Interação lógica para CardDePokemonView.xam
+    /// </summary>
+    public partial class CardDePokemonView : UserControl
+    {
+        public CardDePokemonView()
+        {
+            InitializeComponent();
+            PokemonImage.Source = new BitmapImage(new Uri("https://th.bing.com/th/id/OIP.GfvcTMJ4-_xEdijPRP65WAHaFj?pid=ImgDet&rs=1"));
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
